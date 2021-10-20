6808415d944b5ddb7b684bd832d6d225fbaec60a
AnneCode <anneliu49@gmail.com>
2021-10-20 18:15:12 -0400

    fixbug: index.ts in components

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,23 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "./HomePage.module.scss";
 import {
   Header,
   Footer
 } from "../../components";
-import { Spin, Col, Row } from 'antd';
 import { Categories } from '../../components';
-import axios from 'axios'
-// import { useDispatch } from "react-redux";
-// import { getCategories } from '../../redux/categories/slice';
-// import { useSelector } from "../../redux/hooks";
 
-interface Props { }
-interface State {
-  name: string,
-  ImageSrc: string,
-}
-
-export const HomePage: React.FC = (Props) => {
+export const HomePage: React.FC = () => {
 
   return (<>
     <Header />
