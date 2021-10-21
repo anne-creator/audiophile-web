f26b9409b3d06bd14c54eb28b491cfa313567bfe
AnneCode <anneliu49@gmail.com>
2021-10-20 21:34:30 -0400

    feat: get API from react-redux inside component done: build redux store and etc, with async action

--- src/components/categories/Categories.tsx
+++ src/components/categories/Categories.tsx
@@ -1,31 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect } from 'react'
 import { Row, Col } from 'antd'
 import styles from './Categories.module.scss'
-
+import { RootState } from "../../redux/store";
+import { useSelector } from "../../redux/hooks";
+import { useDispatch } from "react-redux";
+import { Dispatch } from "redux";
 import { CategoryItem } from './CategoryItem'
+import { giveMeDataActionCreator } from '../../redux/categories/categoriesActions';
+
 export const Categories: React.FC = () => {
-    const [list, setList] = useState<any>([]);
-    const [loading, setLoading] = useState<boolean>(false);
-    const [error, setError] = useState<string>()
+
+    /**fetch data from store */
+    const itemList = useSelector(s => s.categoreis.itemList);
+    const error = useSelector(s => s.categoreis.error);
+    const loading = useSelector(s => s.categoreis.loading);
+
+    const dispatch = useDispatch();
 
     useEffect(() => {
-        const getData = async () => {
-            setLoading(true);
-            try {
-                const { data }: any = await axios.get(
-                    'https://www.fastmock.site/mock/456923cc3f54559b181a0f418788fccc/api/categories'
-                )
-                setList(data.data);
-            } catch (e: any) {
-                setError(e.message)
-            }
-        }
-        getData();
+        const fetchData = () => { dispatch(giveMeDataActionCreator()) }
+        fetchData();
     }, [])
     return (<>
         <Row className={styles['category']} gutter={[16, { xs: 32, sm: 64, md: 128, lg: 256 }]}>
-            {list.map((item) => {
+            {itemList.map((item) => {
                 return (
                     <Col className={styles['col']} xs={24} sm={24} md={8} >
                         <CategoryItem title={item.title} imageSrc={item.imageSrc} />
--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import "antd/dist/antd.css";
-// import { store } from 'redux/store';
-// import { Provider } from 'react-redux';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
 
 ReactDOM.render(
   <React.StrictMode>
     {/* connect redux store with app */}
-    {/* <Provider store={store}> */}
-    <App />
-    {/* </Provider> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
