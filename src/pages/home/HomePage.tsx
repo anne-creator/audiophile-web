984ac9a476d58456cfad1a1e18346673d3cfe4ef
Anne <anneliu49@gmail.com>
2021-11-03 20:41:23 -0400

    hero page minor change

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -7,6 +7,7 @@ import { MainLayout } from '../../layouts/mainLayout';
 import styles from './HomePage.module.scss'
 import { Button, Row, Col } from 'antd'
 import hero from '../../assets/hero.png'
+import { Link } from 'react-router-dom'
 
 export const HomePage: React.FC = () => {
   return (<>
@@ -18,11 +19,15 @@ export const HomePage: React.FC = () => {
               <div className={styles['hero__detail__new']}>NEW PRODUCT</div>
               <div className={styles['hero__detail__title']}>XX99 MARK II HEADPHONES</div>
               <div className={styles['hero__detail__des']}>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</div>
-              <Button className={styles['button-primary']} type='primary'>SEE PRODUCT</Button>
+              <Link to={`/productItem/1`}>
+                <Button className={styles['button-primary']} type='primary'>SEE PRODUCT</Button>
+              </Link>
             </div>
           </Col>
-          <Col xs={24} sm={24} md={24} lg={14} className={styles['hero__img-container']} >
-            <img src={hero} alt="XX99" className={styles['hero__img']} />
+          <Col xs={24} sm={24} md={24} lg={14}  >
+            <div className={styles['hero__img-container']} >
+              <img src={hero} alt="XX99" className={styles['hero__img']} />
+            </div>
           </Col>
         </Row>
         <div className={styles['shop']}>
@@ -31,6 +36,5 @@ export const HomePage: React.FC = () => {
         </div>
       </MainLayout>
     </div >
-
   </>)
 }
--- src/pages/home/dist/HomePage.js
+++ src/pages/home/dist/HomePage.js
@@ -7,6 +7,7 @@ var mainLayout_1 = require("../../layouts/mainLayout");
 var HomePage_module_scss_1 = require("./HomePage.module.scss");
 var antd_1 = require("antd");
 var hero_png_1 = require("../../assets/hero.png");
+var react_router_dom_1 = require("react-router-dom");
 exports.HomePage = function () {
     return (react_1["default"].createElement(react_1["default"].Fragment, null,
         react_1["default"].createElement("div", { className: HomePage_module_scss_1["default"]['homePage'] },
@@ -17,9 +18,11 @@ exports.HomePage = function () {
                             react_1["default"].createElement("div", { className: HomePage_module_scss_1["default"]['hero__detail__new'] }, "NEW PRODUCT"),
                             react_1["default"].createElement("div", { className: HomePage_module_scss_1["default"]['hero__detail__title'] }, "XX99 MARK II HEADPHONES"),
                             react_1["default"].createElement("div", { className: HomePage_module_scss_1["default"]['hero__detail__des'] }, "Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast."),
-                            react_1["default"].createElement(antd_1.Button, { className: HomePage_module_scss_1["default"]['button-primary'], type: 'primary' }, "SEE PRODUCT"))),
-                    react_1["default"].createElement(antd_1.Col, { xs: 24, sm: 24, md: 24, lg: 14, className: HomePage_module_scss_1["default"]['hero__img-container'] },
-                        react_1["default"].createElement("img", { src: hero_png_1["default"], alt: "XX99", className: HomePage_module_scss_1["default"]['hero__img'] }))),
+                            react_1["default"].createElement(react_router_dom_1.Link, { to: "/productItem/1" },
+                                react_1["default"].createElement(antd_1.Button, { className: HomePage_module_scss_1["default"]['button-primary'], type: 'primary' }, "SEE PRODUCT")))),
+                    react_1["default"].createElement(antd_1.Col, { xs: 24, sm: 24, md: 24, lg: 14 },
+                        react_1["default"].createElement("div", { className: HomePage_module_scss_1["default"]['hero__img-container'] },
+                            react_1["default"].createElement("img", { src: hero_png_1["default"], alt: "XX99", className: HomePage_module_scss_1["default"]['hero__img'] })))),
                 react_1["default"].createElement("div", { className: HomePage_module_scss_1["default"]['shop'] },
                     react_1["default"].createElement(components_1.Categories, null),
                     react_1["default"].createElement(components_1.RecommandProducts, null))))));
